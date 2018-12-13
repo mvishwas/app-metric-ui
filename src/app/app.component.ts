048a3279da433026987f0966ae420656fcd9a3f0
vishwas <vishwas.muninarayana@emc.com>
2018-12-13 12:05:41 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,114 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+//  const httpOptionss = {
+//    headers: new HttpHeaders({
+//      'Access-Control-Allow-Origin': '*'
+//    })
+//  };
+
+const headers = new HttpHeaders().set("Access-Control-Allow-Origin", "*");
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app-metric';
+  appInfo: any;
+  appHealthDetail: any;
+  appMetricsDetail: any;
+  appEnvDetail: any;
+  
+  //headers = new HttpHeaders().set("Access-Control-Allow-Origin", "*");
+
+  //appBaseUrl='http://10.118.169.49:8077/actuatorShowcase/';
+  appBaseUrl = 'http://localhost:8077/actuatorShowcase/'
+
+  //appInfoUrl = 'http://localhost:8077/actuatorShowcase/info';
+  //appHealthUrl = 'http://localhost:8077/actuatorShowcase/health';
+  //appMetricsUrl = 'http://localhost:8077/actuatorShowcase/metrics';
+  //appEnvUrl = 'http://localhost:8077/actuatorShowcase/env';
+  //appMetricsUrl = 'http://localhost:8787/metrics';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(){
+    console.log("inside init()")
+    this.getAppDetails();
+    this.getAppHealthDetails();
+    this.getAppMetricsDetails();
+    this.getAppEnvDetails();
+  }
+
+  //app info related
+  getAppDetails():void {
+    this.restgetAppDetails()
+    .subscribe(
+      restItems => {
+        this.appInfo = restItems;
+        // console.log(this.appInfo);
+      }
+    )
+  }
+  // Rest Items Service: Read all REST Items
+  restgetAppDetails() {
+    return this.http
+      .get<any[]>(this.appBaseUrl+'info')
+      .pipe(map(data => data));
+  }
+
+// health related 
+  getAppHealthDetails():void {
+    this.restgetAppHealthDetails()
+    .subscribe(
+      restHealthDetails => {
+        this.appHealthDetail = restHealthDetails;
+        // console.log(this.appHealthDetail);
+      }
+    )
+  }
+  
+  restgetAppHealthDetails() {
+    return this.http
+      .get<any[]>(this.appBaseUrl +'health')
+      .pipe(map(data => data));
+  }
+
+//metrics related api 
+getAppMetricsDetails():void {
+  this.restgetAppMetricsDetails()
+  .subscribe(
+    restMetricsDetails => {
+      this.appMetricsDetail = restMetricsDetails;
+      console.log(this.appMetricsDetail);
+    }
+  )
+}
+
+restgetAppMetricsDetails() {
+  return this.http
+    .get<any[]>(this.appBaseUrl+'metrics')
+    .pipe(map(data => data));
+}
+
+//env details
+getAppEnvDetails():void {
+  this.restgetAppEnvDetails()
+  .subscribe(
+    restEnvDetails => {
+      this.appEnvDetail = restEnvDetails;  
+      console.log(this.appEnvDetail);   
+    }
+  )
+}
+
+restgetAppEnvDetails() {
+  return this.http
+    .get<any[]>(this.appBaseUrl+'env')
+    .pipe(map(data => data));
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { HttpClientModule }   from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule }     from './app-routing.module';
+
+import { AppComponent }         from './app.component';
+
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule
+
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+   
+  ],
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
+
+
+/*
+Copyright 2017-2018 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
